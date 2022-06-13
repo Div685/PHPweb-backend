f1542985d59d1afc92c64595addd6f396f8cc376
Div685 <divyeshpatel685@gmail.com>
2022-06-13 16:49:29 +0300

    Update delete.php file

--- php/index.php
+++ php/index.php
@@ -1,29 +1,56 @@
-<?php
-
-$host = 'db';
-$user = 'devuser'; // divUser
-$password = 'devpass'; //G+XwvPc{FDJ2jiYm
-$db = 'test_db';
-
-// $dsn = 'mysql:host='.$this->$host.';dbname='.$this->$db, $this->$user, $this->$password;
-
-// $conn = new mysqli($host,$user,$password,$db);
-try {
-  $conn = new PDO('mysql:host='.$host.';dbname='.$db, $user, $password);
-} catch (PDOException $e) {
-  echo 'Connection Error '.$e->getMessage();
-}
-
-
-//$db->setAttribute(PDO::ATTR_EMULATE_PREPARES, false);
-//$db->setAttribute(PDO::MYSQL_ATTR_USE_BUFFERED_QUERY, true);
-//$db->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-
-if ($conn->connect_error) {
-  echo 'Connection Failed' . $conn->connect_error;
-  exit;
-}
-
-echo 'Succesfuly connected to MySQL';
-
+<?php 
+    session_start();
 ?>
+<!DOCTYPE html>
+<html>
+<head>
+    <title>How To Delete Multiple Records in MySQL Using PHP</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet">
+</head>
+<body class="bg-dark">
+    <div class="container mt-5">
+        <?php 
+            if(isset($_SESSION["msg"]) && !empty($_SESSION["msg"]))
+                {
+                    $msg=$_SESSION["msg"];
+                    echo "<div class='alert alert-danger' role='alert'>".$msg."</div>";
+                    unset($_SESSION['msg']);
+                    session_destroy();
+                }
+        ?>
+        <div class="card">
+            <div class="card-header">
+                <h3 class="text-center">How To Delete Multiple Records in MySQL Using PHP - Mywebtuts.com</h3>
+            </div>
+            <div class="card-body">
+                <form method="POST" action="api/delete.php">
+                    <table class="table table-striped table-bordered table-hover text-center">
+                        <tr>
+                            <th width="10px">#</th>
+                            <th width="10px">id</th>
+                            <th width="50%">name</th>
+                            <th>email</th>
+                        </tr>
+                        <?php
+                            include('conn.php');
+             
+                            $query=mysqli_query($conn,"select * from `products`");
+                            while($row=mysqli_fetch_array($query)){
+                        ?>
+                        <tr>
+                            <td align="center"><input type="checkbox" value="<?php echo $row['pid']; ?>" name="checkbox[]"></td>
+                            <td><?php echo $row['pid']; ?></td>
+                            <td><?php echo $row['SKU']; ?></td>
+                            <td><?php echo $row['Price']; ?></td>     
+                        </tr>
+                        <?php
+                            }
+                        ?>
+                    </table>
+                    <button type="submit" class="btn btn-danger"><span class="glyphicon glyphicon-trash"></span>Delete</button>
+                </form>
+            </div>
+        </div>
+    </div>
+</body>
+</html>
