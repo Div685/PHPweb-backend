f5b43895129d4d12c0e8521cbaf82dda0461c084
Div685 <divyeshpatel685@gmail.com>
2022-06-13 12:40:16 +0300

    Add Delete.php file
    - Add delete function to product API

--- php/models/Products.php
+++ php/models/Products.php
@@ -18,7 +18,7 @@
     // Get Products
 
     public function read() {
-      $query = 'SELECT SKU, Name, Price, Measure FROM '. $this->table .' ORDER BY created_at DESC';
+      $query = 'SELECT pid, SKU, Name, Price, Measure FROM '. $this->table .' ORDER BY created_at DESC';
 
       // Prepare statement
       $stmt = $this->conn->prepare($query);
@@ -78,22 +78,67 @@
         // sanitize
         $ids = htmlspecialchars(strip_tags($ids));
 
-        $query = 'DELETE FROM ' . $this->table . ' WHERE id IN ($ids)';
-        
+        // printf("Err", $ids);
+        $val = "122";
+
+        // $query = 'DELETE FROM ' . $this->table . ' WHERE pid IN ($ids)';
+        $query = 'DELETE FROM products WHERE pid IN ('. $ids .')';
+
+        echo ''.$query;
         // return query
         $stmt = $this->conn->prepare($query);
 
         if($stmt->execute()){
+          echo 'Successfull';
           return true;
+        }else {
+         printf("Error: $s. \n", $stmt->error);
+          return false;
         }
          // Print error if something goes wrong
-         printf("Error: $s. \n", $stmt->error);
+        //  printf("Error: $s. \n", $stmt->error);
 
-         return false;
+        //  return false;
 
-      } catch (PDOException $e) {
-        die('ERROR: '. $e->getMessage());
+      }  catch(PDOException $exception) {
+        die('ERROR: '. $exception->getMessage());
       }
     }
+
+    function deleteMultipleData($conn, $checkedId){
+      try {
+
+        $checkedIdGroup = implode(',', $checkedId);
+        $query = "DELETE FROM products WHERE pid IN ($checkedIdGroup)";
+        echo ''.$query;
+
+        $result = $this->conn->prepare($query);
+        echo $result->execute() ? 'true' : 'False';
+      // $result = $conn->query($query);
+      if($result==true){
+        echo "<h1> Product Deleted Successfully </h1>";
+        echo json_encode(
+          array('message' => 'Product Deleted!')
+        );
+        // header("location:index.php");
+      } else {
+        echo json_encode(
+          array('message' => 'Something Went Wrong!')
+        );
+      }
+       // Execute query
+      // if($result->execute()) {
+      //   return true;
+      // }
+
+      // // Print error if something goes wrong
+      // printf("Error: $s. \n", $result->error);
+
+      // return false;
+      } catch(PDOException $exception) {
+        die('ERROR: '. $exception->getMessage());
+      }
+    }
+
   }
 ?>
